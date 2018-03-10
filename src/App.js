4787e410e86ee5d8565f10f144af70b71a45e40b
Brian Wright <bwright@webstaurantstore.com>
2018-03-09 21:57:25 -0500

    Initial commit of boilerplate react app. Added Card and DrawButton components

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import './App.css';
+import Card from './Card/Card';
+import DrawButton from './DrawButton/DrawButton';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.updateCard = this.updateCard.bind(this);
+
+    this.state = {
+      cards: [
+        {id:1, eng:"English", han:"Hanzi", pin: "Pinyin"},
+        {id:2, eng:"English_2", han:"Hanzi_2", pin: "Pinyin_2"},
+        {id:3, eng:"English_3", han:"Hanzi_3", pin: "Pinyin_3"},
+        {id:4, eng:"English_4", han:"Hanzi_4", pin: "Pinyin_4"},
+        {id:5, eng:"English_5", han:"Hanzi_5", pin: "Pinyin_5"},
+        {id:6, eng:"English_6", han:"Hanzi_6", pin: "Pinyin_6"},
+        {id:7, eng:"English_7", han:"Hanzi_7", pin: "Pinyin_7"}
+      ],
+      currentCard: {}
+    }
+  }
+  
+  componentWillMount(){
+    const currentCards = this.state.cards;
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    });
+  }
+
+  getRandomCard(currentCards){
+    var card = currentCards[Math.floor(Math.random() * currentCards.length)];
+    return card;
+  }
+
+  updateCard(){
+    const currentCards = this.state.cards;
+    this.setState({
+      currentCard: this.getRandomCard(currentCards)
+    });
+    
+    //console.log('New Card');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="cardRow">
+          <Card eng={this.state.currentCard.eng} 
+                  han={this.state.currentCard.han} 
+                  pin={this.state.currentCard.pin}
+                  />
+        </div>
+        <div className="buttonRow">      
+          <DrawButton drawCard={this.updateCard} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Config/Firebase/db_confg.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyCU5RRJginy2IZFYFkx3rAkUaeEeS3kilk",
+    authDomain: "react-flashcards-d7e37.firebaseapp.com",
+    databaseURL: "https://react-flashcards-d7e37.firebaseio.com",
+    projectId: "react-flashcards-d7e37",
+    storageBucket: "",
+    messagingSenderId: "1080280546729"
+}
