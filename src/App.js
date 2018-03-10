3dfd6fa9178b0b28425dc56b0c54da77fceb2c68
Brian Wright <bwright@webstaurantstore.com>
2018-03-10 08:23:41 -0500

    Styling. Firebase database implemented.

--- src/App.js
+++ src/App.js
@@ -2,34 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import Card from './Card/Card';
 import DrawButton from './DrawButton/DrawButton';
-
-import { DB_CONFIG } from './Config/Firebase/db_config'
+import { DB_CONFIG } from './Config/Firebase/db_config';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 class App extends Component {
   constructor(props){
     super(props);
 
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref().child('cards');
     this.updateCard = this.updateCard.bind(this);
 
     this.state = {
-      cards: [
-        {id:1, eng:"English", han:"Hanzi", pin: "Pinyin"},
-        {id:2, eng:"English_2", han:"Hanzi_2", pin: "Pinyin_2"},
-        {id:3, eng:"English_3", han:"Hanzi_3", pin: "Pinyin_3"},
-        {id:4, eng:"English_4", han:"Hanzi_4", pin: "Pinyin_4"},
-        {id:5, eng:"English_5", han:"Hanzi_5", pin: "Pinyin_5"},
-        {id:6, eng:"English_6", han:"Hanzi_6", pin: "Pinyin_6"},
-        {id:7, eng:"English_7", han:"Hanzi_7", pin: "Pinyin_7"}
-      ],
+      cards: [],
       currentCard: {}
     }
   }
   
   componentWillMount(){
     const currentCards = this.state.cards;
-    this.setState({
-      cards: currentCards,
-      currentCard: this.getRandomCard(currentCards)
+
+    this.database.on('child_added', snap => {
+      currentCards.push({
+        id: snap.key,
+        eng: snap.val().eng,
+        han: snap.val().han,
+        pin: snap.val().pin
+      });
+      this.setState({
+        cards: currentCards,
+        currentCard: this.getRandomCard(currentCards)
+      });
     });
   }
 
